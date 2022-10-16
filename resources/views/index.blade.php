7ca488edcddbe9998ba84fb4f2d983b89025933d
sawagenaar <sadanilova@hotmail.com>
2022-10-17 00:14:54 +0200

    checking_for_null

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,49 +4,61 @@
 @section('content')
 <div class="container">
   {{-- Promotie beschrijving --}}
-  <div class="description offer-description">
-    <h2 class="title">{{ $sale->title }}</h2>
-    <h3 class="subtitle">{{ $sale->shortdescription }}</h3>
-    <h3 class="subtitle">{{ $sale->fulldescription }}</h3>
-    <a class="link" href="#">Lees verder</a>
-  </div>
-  <div class="photo">
-    <img class="photo-exl" src="{{ asset('storage/'.$sale->image) }}">
-  </div>
+  @if (count($sale))
+    <div class="description offer-description">
+      <h2 class="title">{{ $sale->title }}</h2>
+      <h3 class="subtitle">{{ $sale->shortdescription }}</h3>
+      <h3 class="subtitle">{{ $sale->fulldescription }}</h3>
+      <a class="link" href="#">Lees verder</a>
+    </div>
+    <div class="photo">
+      <img class="photo-exl" src="{{ asset('storage/'.$sale->image) }}">
+    </div>
+  @else
+    <div class="cart-message">Geen aanbieding</div>
+  @endif
 </div>
 {{-- Afgeprijsde producten --}}
 <div class="container">
-  <ul class="sales-list">
-    @foreach($discounts as $discount)
-    <li class="sales-item">
-      <div class="sale-description"><a class="link" href="{{ route('pages.product', $discount->slug) }}">
-        <h2 class="title">{{ $discount->name }}</h2>
-        <p class="info">{{ $discount->shortdescription }}</p>
-        <p class="info">{{ $discount->fulldescription }}</p>
-        </a>
-      </div>
-      <div class="product-item">
-        <div class="item">
-          <img class="photo-md" src="{{ asset('storage/'.$discount->image) }}">
+  @if (count($discounts))
+    <ul class="sales-list">
+      @foreach($discounts as $discount)
+      <li class="sales-item">
+        <div class="sale-description"><a class="link" href="{{ route('pages.product', $discount->slug) }}">
+          <h2 class="title">{{ $discount->name }}</h2>
+          <p class="info">{{ $discount->shortdescription }}</p>
+          <p class="info">{{ $discount->fulldescription }}</p>
+          </a>
         </div>
-        <div class="price-block">
-          <del class="price subcategory-price">{{ number_format($discount->price, 2, '.', '') }}</del>
-          <span class="price-discount subcategory-price">{{ number_format($discount->discounted_price, 2, '.', '') }}</span>
+        <div class="product-item">
+          <div class="item">
+            <img class="photo-md" src="{{ asset('storage/'.$discount->image) }}">
+          </div>
+          <div class="price-block">
+            <del class="price subcategory-price">{{ number_format($discount->price, 2, '.', '') }}</del>
+            <span class="price-discount subcategory-price">{{ number_format($discount->discounted_price, 2, '.', '') }}</span>
+          </div>
         </div>
-      </div>
-    </li>
-    @endforeach
-  </ul>
+      </li>
+      @endforeach
+    </ul>
+  @else
+    <div class="cart-message">Geen discounts</div>
+  @endif
 </div>
 {{-- News --}}
 <div class="container news-container">
-  <div class="description news-description">
-    <h2 class="title">{{ $news->title }}</h2>
-    <p class="info">{{ $news->shortdescription }}</p>
-    <a class="link" href="#">Lees verder</a>
-  </div>
-  <div class="photo">
-    <img class="photo-exl" src="{{ asset('storage/'.$news->image) }}">
-  </div>
+  @if (count($news))
+    <div class="description news-description">
+      <h2 class="title">{{ $news->title }}</h2>
+      <p class="info">{{ $news->shortdescription }}</p>
+      <a class="link" href="#">Lees verder</a>
+    </div>
+    <div class="photo">
+      <img class="photo-exl" src="{{ asset('storage/'.$news->image) }}">
+    </div>
+  @else
+    <div class="cart-message">Geen news</div>
+  @endif
 </div>
 @endsection
